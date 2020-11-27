4be1bf9a411b0ac06e370299e26867ebbd426c4e
Prakash <prakash106kumar@gmail.com>
2020-11-27 15:57:02 +0530

    service worker & web worker excercies

---
+++ web-worker-exercise/web/js/worker.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var curFib = 0;
+
+self.onmessage = onMessage;
+// **********************************
+
+function onMessage(event) {
+  getNextFib();
+}
+
+function getNextFib() {
+  var fibNum = fib(curFib);
+  self.postMessage({ fib: fibNum, idx: curFib });
+  curFib++;
+  setTimeout(getNextFib, 0);
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
