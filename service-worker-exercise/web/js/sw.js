3704ae2749be03704bdc8f668944f6e7310bdc12
Prakash <prakash106kumar@gmail.com>
2020-11-28 18:58:00 +0530

    feat: cache the assets

--- service-worker-exercise/web/js/sw.js
+++ service-worker-exercise/web/js/sw.js
@@ -1,8 +1,27 @@
 "use strict";
 
-const version = 2;
+const version = 3;
 var isOnline = true;
 var isLoggedIn = false;
+var cacheName = `ramblings-${version}`
+
+var urlsToCache = {
+    loggedOut: [
+        "/",
+        "/about",
+        "/contact",
+        "/login",
+        "/404",
+        "/offline",
+        "/css/style.css",
+        "/js/blog.js",
+        "/js/home.js",
+        "/js/login.js",
+        "/js/add-post.js",
+        "/images/logo.gif",
+        "/images/offline.png"
+    ]
+}
 
 self.addEventListener("install", onInstall);
 self.addEventListener("activate", onActivate);
@@ -14,6 +33,7 @@ main().catch(console.error);
 
 async function main() {
     await sendMessage({ requestStatusUpdate: true });
+    await cacheLoggedOutFiles();
 }
 
 async function onInstall() {
@@ -48,6 +68,40 @@ async function onActivate(event) {
 
 async function handleActivation() {
     await clients.claim();
+    await cacheLoggedOutFiles(/*forceReload=*/ true);
     console.log(`Service Worker (${version}) is started....`);
 }
 
+async function cacheLoggedOutFiles(forceReload = false) {
+    //open that cache entry
+    var cache = await caches.open(cacheName);
+    
+    return Promise.all(
+        urlsToCache.loggedOut.map(async function requestFile(url) {
+            try {
+                let res;
+
+                if (!forceReload) {
+                    res = await cache.match(url);
+                    if (res) {
+                        return res;
+                    }
+                }
+
+                let fetchOptions = {
+                    method: "GET",
+                    cache: "no-cache",
+                    credentials: "omit"
+                }
+
+                res = await fetch(url, fetchOptions);
+                if (res.ok) {
+                    await cache.put(url, res);
+                }
+
+            } catch (error) {
+                
+            }
+        })
+    )
+}
